bf4195f5f6a6ca592b5cddb62cf7cee8ccce57bc
Rafael-R <rsrodrigues.99@gmail.com>
2025-05-18 20:18:39 +0100

    fix: knex configuration to start database

--- backend/src/database/knexfile.js
+++ backend/src/database/knexfile.js
@@ -2,7 +2,7 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './src/database/todos.db3'
+      filename: './todos.db3'
     },
     useNullAsDefault: true,
     migrations: {
